0329fea32a0d3f7dafa807236a669ece93f42aa1
Rui Silva <Rui.Silva@devscope.net>
2018-07-13 17:42:25 +0100

    First commit

---
+++ Module1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Module1
+{
+    public class Class1
+    {
+    }
+}
---
+++ Module1/Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "OrchardCore.Modules.Module1",
+    Author = "Devscope Products Team",
+    Website = "http://devscope.net",
+    Version = "0.0.1",
+    Description = "Template module."
+)]
